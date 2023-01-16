741e1e6206931e6555b87914bc2b0d8f701f99c5
Vandom <ahmadi.mikaeel@gmail.com>
2023-01-17 01:35:58 +0330

    add Comment,form,....204 finish

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Products
+from .forms import CommentForm
+from .models import Products, Comment
 
 
 class ProductListView(generic.ListView):
@@ -15,3 +16,18 @@ class ProductDetailView(generic.DetailView):
     model = Products
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form = CommentForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        return super().form_valid(form)

a8912a55c620c268f5c8d77e7543c111ab7b6aeb
Vandom <ahmadi.mikaeel@gmail.com>
2023-01-24 02:51:44 +0330

    translation for templates

--- products/models.py
+++ products/models.py
@@ -34,8 +34,8 @@ class Comment(models.Model):
     ]
     product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='comments', )
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
-    body = models.TextField()
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    body = models.TextField(verbose_name='Comment Text')
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS,verbose_name='Your Score',)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
